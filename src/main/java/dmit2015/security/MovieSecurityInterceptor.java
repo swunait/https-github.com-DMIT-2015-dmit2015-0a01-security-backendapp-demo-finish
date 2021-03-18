1d48645fe13fd8063b14d6acbf94081c41be0350
Sam Wu <swu@nait.ca>
2021-03-18 15:23:05 -0600

    End of March 18 demo

---
+++ src/main/java/dmit2015/security/MovieSecurityInterceptor.java
@@ -0,0 +1,41 @@
+package dmit2015.security;
+
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.InvocationContext;
+import javax.security.enterprise.SecurityContext;
+import java.util.Locale;
+import java.util.logging.Logger;
+
+public class MovieSecurityInterceptor {
+
+    @Inject
+    private Logger _logger;
+
+    private SecurityContext _securityContext;
+
+    @AroundInvoke
+    public Object verifyAccess(InvocationContext ic) throws Exception {
+        String methodName = ic.getMethod().getName();
+        _logger.info("Entering method " + methodName);
+        // Only authenticated users with the role USER or Sales can execute the update or add method
+        // Only authenticated users with the role ADMIN or Executive or Adminstration can execute the delete method
+        if (_securityContext.getCallerPrincipal() == null) {
+            throw new RuntimeException("Access denied!. Only authenticated users have permission to execute this method.");
+        }
+        if (methodName.toLowerCase().startsWith("add") || methodName.toLowerCase().startsWith("update")) {
+            if (!_securityContext.isCallerInRole("USER") && !_securityContext.isCallerInRole("Sales")) {
+                throw new RuntimeException("Access denied!. You do not have permission to execute this method.");
+            }
+        }
+        if (methodName.toLowerCase().startsWith("remove") ) {
+            if (!_securityContext.isCallerInRole("ADMIN")
+                    && !_securityContext.isCallerInRole("Executive")
+                    && !_securityContext.isCallerInRole("Administration")) {
+                throw new RuntimeException("Access denied!. You do not have permission to execute this method.");
+            }
+        }
+
+        return ic.proceed();
+    }
+}
--- src/main/java/dmit2015/view/MovieCreateController.java
+++ src/main/java/dmit2015/view/MovieCreateController.java
@@ -1,6 +1,7 @@
 package dmit2015.view;
 
 import dmit2015.entity.Movie;
+import dmit2015.producer.LoggerProducer;
 import dmit2015.repository.MovieRepository;
 import lombok.Getter;
 import org.omnifaces.util.Messages;
@@ -8,10 +9,15 @@ import org.omnifaces.util.Messages;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.logging.Logger;
 
 @Named("currentMovieCreateController")
 @RequestScoped
 public class MovieCreateController {
+
+//    private Logger logger = Logger.getLogger(MovieCreateController.class.getName());
+    @Inject
+    private Logger logger;
 
     @Inject
     private MovieRepository _movieRepository;
@@ -20,6 +26,7 @@ public class MovieCreateController {
     private Movie newMovie = new Movie();
 
     public String onCreateNew() {
+        logger.info("entering onCreateNew method");
         String nextPage = "";
         try {
             _movieRepository.add(newMovie);
@@ -27,8 +34,10 @@ public class MovieCreateController {
             nextPage = "index?faces-redirect=true";
         } catch (Exception e) {
             e.printStackTrace();
+            logger.fine(e.getMessage());
             Messages.addGlobalError("Create was not successful.");
         }
+        logger.info("exiting onCreateNewMethod");
         return nextPage;
     }
 
